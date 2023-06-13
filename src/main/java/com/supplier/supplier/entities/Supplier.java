eaf238745f6fc42b7e6dd9a6a01e93afceaad499
felipecruz159 <felipe.cruz159@hotmail.com>
2023-06-13 08:28:09 -0300

    crud complete

--- src/main/java/com/supplier/supplier/entities/Supplier.java
+++ src/main/java/com/supplier/supplier/entities/Supplier.java
@@ -3,6 +3,8 @@ package com.supplier.supplier.entities;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ import jakarta.persistence.Table;
 public class Supplier implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
--- src/main/java/com/supplier/supplier/services/SupplierService.java
+++ src/main/java/com/supplier/supplier/services/SupplierService.java
@@ -25,4 +25,18 @@ public class SupplierService {
     public List<Supplier> getSuppliers(){
         return supplierRepository.findAll();
     }
+
+    public void deleteById(int id) {
+        Supplier supplier = getSupplierById(id);
+        supplierRepository.delete(supplier);
+    }
+
+    public Supplier save(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    public void update(int id, Supplier supplier) {
+        getSupplierById(id);
+        supplierRepository.save(supplier);
+    }
 }
